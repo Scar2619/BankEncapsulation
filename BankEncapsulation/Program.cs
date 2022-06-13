5e0d7d31301d16f433274887287145613b78299f
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-13 15:12:08 -0400

    finalized

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var newDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(newDeposit);
+
+            Console.WriteLine($"You deposited {newDeposit}. Your new balance is {account.GetBalance()}.");
+        }
+
+    }
+}
+
